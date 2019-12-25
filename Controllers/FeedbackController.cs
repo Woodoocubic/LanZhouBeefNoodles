38e164970af217132edc55d2440af1b97af0b9e0
Woodoocubic <guangqingliu1985@gmail.com>
2019-12-25 20:12:52 +0800

    validation for feedbacks
    almost forget taghelper

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -2,13 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LanzhouBeefNoodles.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanzhouBeefNoodles.Controllers
 {
     public class FeedbackController : Controller
     {
+        private IFeedbackRepository _feedbackRepository;
+
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            //taghelper
+            if (ModelState.IsValid)
+            {
+                _feedbackRepository.AddFeedback(feedback);
+                return RedirectToAction("FeedbackComplete");
+            }
+            return View();
+        }
+
+        public IActionResult FeedbackComplete()
         {
             return View();
         }
--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace LanzhouBeefNoodles.Models
 {
     public class Feedback
     {
+        [BindNever]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "pls write down your name")]
+        [StringLength(50, ErrorMessage = "You name must be less than 50")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is good")]
+        [StringLength(50,ErrorMessage = "email is not that long")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "your email is wrong")]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+            ErrorMessage = "pls type in the right email")]
         public string Email { get; set; }
-        public DateTime CreateDateUTC { get; set; }
+        
+        [Required(ErrorMessage = "pls, we need your feedback")]
+        [StringLength(400, ErrorMessage = "cannot exceed 400 characters")]
         public string Message { get; set; }
+
+        public DateTime CreateDateUTC { get; set; }
     }
+
 }
